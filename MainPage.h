ac467b57e961a43a3bf61e7c0e125ffef67a461e
charelstoncrabb <ncrabb@ucdavis.edu>
2018-07-28 22:00:48 -0700

    Added hyperlink to opt namespace from main page docs

--- MainPage.h
+++ MainPage.h
@@ -2,5 +2,13 @@
 	\author Nicholas Crabb
 
 \mainpage basic_opt: a simple C++ optimization library
- This library is intended to provide a simple, user-friendly C++ black-box optimization library.  The current user-interface is through a single header file opt.h.  The optimization functions are within in the <b>opt</b> namespace.  Currently implemented is a simple <A HREF="https://en.wikipedia.org/wiki/Gradient_descent">gradient descent method</A> for single- or multi-variate functions.  Up next, <A HREF="https://en.wikipedia.org/wiki/Conjugate_gradient_method">conjugate gradient methods</A>.  The technical roadmap for this project includes writing an optimization library wrapper for the <A HREF="http://eigen.tuxfamily.org/index.php?title=Main_Page">Eigen</A> linear algebra library.
+ This library is intended to provide a simple, user-friendly C++ black-box optimization library.
+ 
+ The current user-interface is through a single header file opt.h.
+ 
+ The optimization functions are within in the <A HREF="https://charelstoncrabb.github.io/basic_opt/html/namespaceopt.html">opt</A> namespace.
+ 
+ Currently implemented is a simple <A HREF="https://en.wikipedia.org/wiki/Gradient_descent">gradient descent method</A> for single- or multi-variate functions.
+ 
+ Up next, <A HREF="https://en.wikipedia.org/wiki/Conjugate_gradient_method">conjugate gradient methods</A>.  The technical roadmap for this project includes writing an optimization library wrapper for the <A HREF="http://eigen.tuxfamily.org/index.php?title=Main_Page">Eigen</A> linear algebra library.
 */
