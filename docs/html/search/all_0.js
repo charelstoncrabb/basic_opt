79d0b96d9cf5cda68fd526b8cedfe30cb44c2691
charelstoncrabb <ncrabb@ucdavis.edu>
2018-07-28 20:22:11 -0700

    Adding main page documentation to html docs

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['basic_5fopt_3a_20a_20simple_20c_2b_2b_20optimization_20library',['basic_opt: a simple C++ optimization library',['../index.html',1,'']]],
   ['basic_5fopt',['basic_opt',['../md__r_e_a_d_m_e.html',1,'']]]
 ];
